5a33582de7f9a1c742954e195ce5b6acb933e8a1
brunacmelo <brunacmelo14@gmail.com>
2020-10-20 10:27:57 -0300

    Commit10

--- src/Movie.java
+++ src/Movie.java
@@ -44,4 +44,15 @@ public class Movie {
     }
   return thisAmount;
   }
+
+  public int getFrequentRenterPoints(int daysRented) {
+    int frequentRenterPoints = 0;
+        // add frequent renter points
+    frequentRenterPoints ++;
+
+    if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)  
+      frequentRenterPoints ++;
+    return frequentRenterPoints;
+  }
+
 }
